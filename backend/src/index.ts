7b39536c48322a09e6bc482e7dee64cc62c738ab
DeathwingIN <imeshnirmal1u@gmail.com>
2024-07-17 10:00:17 +0530

    User test API Created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,19 @@
-import express, {Request, Response} from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
-import  mongoose from "mongoose";
-
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoutes";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
-    console.log("Connected to MongoDB");
-})
+  console.log("Connected to MongoDB");
+});
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-app.get("/test", async (req: Request, res: Response) => {
-
-    res.json({message: "API is working"});
-
-
-});
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
-    console.log("Server is running on port 7000");
+  console.log("Server is running on port 7000");
 });
